1da2bf115bf8401d981ce309bd53cdd869cf74f7
wtoloza <wtoloza@example.com>
2025-09-17 09:47:35 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,9 +25,9 @@ export default defineConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use 'src/styles/variables.scss' as *;`
-        }
-      }
-    }
+          additionalData: `@use 'src/styles/variables.scss' as *;`,
+        },
+      },
+    },
   },
 });
